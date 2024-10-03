6b34094d4eb8a7a8c66ab49df7fa8e0f838cb172
Airenik Melkonyan <melkonyanair@outlook.com>
2024-10-03 16:38:54 +0300

    feat: added models Balance and Transaction

--- apps/balance/models.py
+++ apps/balance/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Balance(models.Model):
+    owner = models.OneToOneField(
+        to="users.User",
+        on_delete=models.CASCADE,
+        related_name="user_balance",
+    )
+    amount = models.PositiveSmallIntegerField(
+        default=0,
+        blank=False,
+        null=False,
+    )
+
+
+class Transaction(models.Model):
+    sender = models.ForeignKey(
+        to="users.User",
+        on_delete=models.CASCADE,
+        related_name="sender_balance",
+    )
+    receiver = models.ForeignKey(
+        to="users.User",
+        on_delete=models.CASCADE,
+        related_name="receiver_balance",
+    )
+    amount = models.PositiveSmallIntegerField(
+        default=0,
+        blank=False,
+        null=False,
+    )
