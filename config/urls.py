26b5057ca29a7dc611f8102e53fa3a8c7b16af29
Airenik Melkonyan <melkonyanair@outlook.com>
2024-10-03 16:36:14 +0300

    feat: refactoring balance app and added urls

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("apps.balance.rest.urls")),
     path("api/", include("apps.base.rest.urls")),
     path("api/", include("apps.users.rest.urls")),
 ]
