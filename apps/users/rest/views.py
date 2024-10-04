ddd3aa44df7ba23b800da063c7c3c3a30033f806
Airenik Melkonyan <melkonyanair@outlook.com>
2024-10-04 12:21:51 +0300

    feat: added description for me method delete

--- apps/users/rest/views.py
+++ apps/users/rest/views.py
@@ -1,5 +1,21 @@
+from djoser.serializers import UserDeleteSerializer
 from djoser.views import UserViewSet as DjoserUserViewSet
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.decorators import action
 
 
 class UserViewSet(DjoserUserViewSet):
-    pass
+    @swagger_auto_schema(
+        method="delete",
+        request_body=UserDeleteSerializer,
+        responses={status.HTTP_204_NO_CONTENT: "No Content"},
+    )
+    @action(
+        detail=False,
+        methods=["get", "put", "patch", "delete"],
+        url_path="me",
+    )
+    def me(self, request, *args, **kwargs):
+        """This is done so that it is possible to enter current_password in swagger."""
+        return super().me(request, *args, **kwargs)
