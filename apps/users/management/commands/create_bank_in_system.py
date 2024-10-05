2f053c83d17e4b0ca2814cce569f37a6eca25b06
Airenik Melkonyan <melkonyanair@outlook.com>
2024-10-05 15:19:46 +0300

    feat: added command to create a bank

---
+++ apps/users/management/commands/create_bank_in_system.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand
+
+from apps.users.models import User
+
+
+class Command(BaseCommand):
+    help = "Creates bank in system"
+
+    def handle(self, *args, **options):
+
+        if User.objects.filter(username="Main bank").exists():
+            self.stdout.write(self.style.NOTICE("Main bank already exists"))
+            return
+
+        user = User.objects.create(
+            username="Main bank",
+            email="bankOOOSPb@localhost",
+            is_staff=True,
+            is_active=True,
+            is_superuser=True,
+        )
+
+        user.set_password("123")
+        user.save()
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                'Created user with username "Main bank" and password "123"'
+            )
+        )
