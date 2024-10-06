0cf1927aa5dbefd51718dfdfc0bb699ac3cb98bb
Airenik Melkonyan <melkonyanair@outlook.com>
2024-10-06 22:17:21 +0300

    feat: added description README.md

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
-DB_HOST = os.environ.get("DB_HOST")
+DB_HOST = os.environ.get("DB_HOST", "localhost")
 DB_PORT = os.environ.get("DB_PORT")
 
 DATABASES = {
